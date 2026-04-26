0bac2cd43ba02e969f462fa984c1093ba7ce541d
flutterdev_babu <flutterdev.babu@gmail.com>
2026-04-27 00:15:07 +0530

    updates in the system with resolved errors

--- backend/server/routes/student.routes.js
+++ backend/server/routes/student.routes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { createStudent, updateStudent, deleteStudent, getStudentQRCodes, bulkCreateStudents, getStudentDashboard } from "../controllers/student.controller.js";
-import { createStudent, updateStudent, deleteStudent, getStudentQRCodes } from "../controllers/student.controller.js";
+
 import { authenticateJWT, authorizeRole } from "../middleware/auth.js";
 
 const router = express.Router();
@@ -11,9 +11,4 @@ router.get("/dashboard/:roll_no", authenticateJWT, getStudentDashboard);
 router.put("/:id", authenticateJWT, updateStudent);
 router.delete("/:id", authenticateJWT, deleteStudent);
 router.get("/:id/qrcodes", authenticateJWT, getStudentQRCodes);
-router.post("/", createStudent);
-router.put("/:id", updateStudent);
-router.delete("/:id", deleteStudent);
-router.get("/:id/qrcodes", getStudentQRCodes);
-
 export default router;
